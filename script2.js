64980d62686a234c7a70f6db5bc8f75bd5921152
sameerkhan0077 <113612008+sameerkhan0077@users.noreply.github.com>
2023-02-07 11:41:47 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,69 @@
+let opreater =prompt("choice your number opretar?");
+let num = Number(prompt("Enter your Number?"));
+let num1 = Number(prompt("Enter your second Number?"));
+switch(opreater){
+	case "+":
+		console.log(num+num1);
+		break;
+		case "-":
+			console.log(num-num1);
+			break;
+			case "*":
+				console.log(num*num1);
+				break;
+				case " /":
+					console.log(num/num1);
+					break;
+					default:
+						console.log("you didn't give any number")
+
+
+}
+
+
+let i = "english";
+switch (i) {
+  case "trigno":
+  case "apti":
+  case "math":
+  case "quant":
+    console.log("mathmatics");
+    break;
+  case true:
+    console.log("JavaScript");
+    break;
+  case false:
+    console.log("Java");
+    break;
+  case "Turkey":
+  case "India":
+  case "Japan":
+    console.log("Country");
+    break;
+  case "english":
+  case "tense":
+  case "active":
+  case "passive":
+    console.log("english");
+    break;
+  case "delhi":
+    console.log("capital");
+    break;
+  case 18:
+    console.log("adult");
+    break;
+  case "rajasthan":
+    console.log("biggest state");
+    break;
+  case 11:
+    console.log("minor");
+    break;
+  default:
+    console.log("hello javaScript");
+}
+ 
+
+
+
+
+
